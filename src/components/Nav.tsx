ded557de10ce30e20a5afa090ea0e0d6c9395c82
amekatze <merudarko@gmail.com>
2023-09-24 18:01:29 +0900

    custom mouse cursor

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,33 @@
-export default function Nav() {
+interface NavProps {
+  changeCursorColor: () => void;
+}
+
+export default function Nav({ changeCursorColor }) {
   return (
-    <div className="nav fixed top-0 w-full border-b-[1.5px] px-6 py-4 flex justify-between font-semibold">
-      <span>
+    <div className="nav fixed top-0 w-full border-b-[1.5px] px-6  flex justify-between font-semibold">
+      <span className="py-6">
         <a href="">MALI KLEINEWEGEN</a>
       </span>
       <ul className="flex justify-between gap-8">
-        <li>
+        <li
+          className="py-6 px-2"
+          onMouseEnter={() => changeCursorColor("hover")}
+          onMouseLeave={() => changeCursorColor("default")}
+        >
           <a href="">my work</a>
         </li>
-        <li>
+        <li
+          className="py-6 px-2"
+          onMouseEnter={() => changeCursorColor("hover")}
+          onMouseLeave={() => changeCursorColor("default")}
+        >
           <a href="">about me</a>
         </li>
-        <li>
+        <li
+          className="py-6 px-2"
+          onMouseEnter={() => changeCursorColor("hover")}
+          onMouseLeave={() => changeCursorColor("default")}
+        >
           <a href="">info</a>
         </li>
       </ul>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { Raleway, Fraunces } from "next/font/google";
+import React, { useEffect } from "react";
 
 import Nav from "@/components/Nav";
 import Header from "@/components/Header";
@@ -12,24 +12,51 @@ const raleway = Raleway({ subsets: ["latin"] });
 const fraunces = Fraunces({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    const circularCursor = document.getElementById("circularcursor");
+
+    const handleMouseMove = (e: any) => {
+      circularCursor.style.left = e.pageX + "px";
+      circularCursor.style.top = e.pageY + "px";
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const changeCursorColor = (color) => {
+    const circularCursor = document.getElementById("circularcursor");
+    if (color === "hover") {
+      circularCursor.classList.add("circularcursor-hover");
+    } else {
+      circularCursor.classList.remove("circularcursor-hover");
+    }
+  };
   return (
     <main
       className={`flex min-h-screen flex-col items-center py-24 px-32 ${raleway.className}`}
     >
-      <Nav />
+      <div id="circularcursor"> </div>
+      <div
+        id="myHoverDiv"
+        className="hoverable-div"
+        onMouseOver={() => changeCursorColor("hover")}
+        onMouseLeave={() => changeCursorColor("default")}
+      ></div>
+      <Nav changeCursorColor={changeCursorColor} />
       <Header fraunces={fraunces} />
       <Work fraunces={fraunces} />
       <About fraunces={fraunces} />
       <Info fraunces={fraunces} />
       <Footer />
-
       <svg width="0" height="0">
         <filter id="grainy-blur" x="-150%" y="-150%" width="400%" height="400%">
           <feGaussianBlur stdDeviation="49" result="blur" />
         </filter>
       </svg>
-      <div className="cursor"></div>
-      <div className="cursor-border"></div>
     </main>
   );
 }
