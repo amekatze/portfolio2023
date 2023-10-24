24f9aae2725e897bb50ed3e8c28eda4a079c014c
amekatze <merudarko@gmail.com>
2023-10-24 21:56:34 +0900

    feat: project updates and image gallery

--- src/pages/projects/[project].tsx
+++ src/pages/projects/[project].tsx
@@ -1,6 +1,8 @@
+"use client";
+
+import { useState } from "react";
 import { Fjalla_One } from "next/font/google";
 import { useRouter } from "next/router";
-import Layout from "@/components/Layout";
 import Image from "next/image";
 import Link from "next/link";
 import projects from "../../projects.json";
@@ -14,6 +16,25 @@ export default function Project() {
   const projectId = parseInt(project_id as string, 10);
   const project = projects.find((project) => project.id === projectId);
 
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const goToNextImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex + 1) % project.images.length
+    );
+  };
+
+  const goToPrevImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + project.images.length) % project.images.length
+    );
+  };
+
+  const handleMiniImageClick = (index) => {
+    setCurrentImageIndex(index);
+  };
+
   return (
     <div className="px-6 md:px-20 mt-28 py-10 pb-20 work">
       <Link href="/#work" className="text-xl">
@@ -24,18 +45,56 @@ export default function Project() {
         {project ? project.title : "Project Not Found"}
       </h1>
       {project && (
-        <div className="flex flex-col justify-center items-center w-full">
-          {project.images.map((image) => (
+        <div className="flex flex-col justify-center items-center w-full relative">
+          <p className="text-xl mb-2">Project Images</p>
+          <div className="relative flex items-center justify-center gap-6">
+            {project.images.length > 1 && (
+              <Image
+                src="/icons/arrow.svg"
+                width={30}
+                height={30}
+                alt="Project Image"
+                onClick={goToPrevImage}
+                className="rounded-md mb-2 cursor-pointer rotate-180 icons"
+              />
+            )}
             <Image
-              src={image}
+              src={project.images[currentImageIndex]}
               width={800}
               height={800}
               alt="Project Image"
               className="rounded-md mb-2"
             />
-          ))}
-          <p>{project.description}</p>
-          <div className="flex gap-4 mt-4">
+            {project.images.length > 1 && (
+              <Image
+                src="/icons/arrow.svg"
+                width={30}
+                height={30}
+                alt="Project Image"
+                onClick={goToNextImage}
+                className="rounded-md mb-2 cursor-pointer  icons"
+              />
+            )}
+          </div>
+          {project.images.length > 1 && (
+            <div className="flex flex-wrap justify-center flex-wrap gap-2">
+              {project.images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={`Mini Image ${index}`}
+                  className={`hover:brightness-50 md:w-28 w-20 cursor-pointer ${
+                    index === currentImageIndex ? "selected" : ""
+                  } `}
+                  onClick={() => handleMiniImageClick(index)}
+                />
+              ))}
+            </div>
+          )}
+          <div className="mt-20">
+            <p>{project.description}</p>
+          </div>
+          <div className="flex gap-4 mt-2">
             {project.live && (
               <a href={project.live} target="_blank" rel="noopener noreferrer">
                 <div className="project-link w-20 h-20 rounded-full flex items-center justify-center">
